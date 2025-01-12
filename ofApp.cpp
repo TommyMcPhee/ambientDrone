9298dbbd2d3bae999ccc82563f7b593a17518935
Tommy McPhee <135555457+TommyMcPhee@users.noreply.github.com>
2025-01-12 00:35:04 -0700

    Add files via upload

--- ofApp.cpp
+++ ofApp.cpp
@@ -5,14 +5,37 @@ constexpr void ofApp::fillWavetable() {
 	for (int a = 0; a < wavetableSize; a++) {
 		wavetable[a] = sin(TWO_PI * (float)a / wavetableSize);
 	}
+	primes[0] = true;
+	primes[1] = true;
+	for (int a = 2; a < limit; a++) {
+		primes[a] = true;
+		for (int b = a + 1; b < limit; b++) {
+			if (b % a == 0) {
+				primes[b - 1] = false;
+			}
+		}
+	}
 }
 
 constexpr const int ofApp::getBankSize(int order) {
 	int size = 0;
-	for (int a = 3; a < degree + 1; a++) {
-		size++;
+	for (int a = 2; a < limit; a++) {
+		//size++;
+		bool aPrime = true;
 		for (int b = 2; b < a; b++) {
-			if (a % b != 0) {
+			if (a % b == 0) {
+				aPrime = false;
+			}
+		}
+		for (int b = 1; b < a; b++) {
+			bool bPrime = true;
+			for (int c = 2; c < b; c++) {
+				if (b % c == 0) {
+					bPrime = false;
+					c = b;
+				}
+			}
+			if (aPrime || bPrime && a % b != 0) {
 				size++;
 			}
 		}
@@ -34,22 +57,41 @@ void ofApp::hardwareSetup() {
 	stream.setup(streamSettings);
 }
 
-void ofApp::setup(){
+void ofApp::setup() {
 	fillWavetable();
-	const int bankSize = getBankSize(degree);
-	cout << bankSize << endl;
+	for (int a = 0; a < limit; a++) {
+		//cout << endl;
+	}
+	//const array<bool, limit> primesConstant = primes;
+	const int bankSize = getBankSize(limit);
+	//cout << bankSize << endl;
 	array<float, bankSize> oscillators;
 	oscillators[0] = 1.0;
-	oscillators[1] = 2.0;
-	oscillators[2] = 0.5;
-	for (int a = 3; a < degree + 1; a++) {
+	//oscillators[1] = 2.0;
+	//oscillators[2] = 0.5;
+	for (int a = 2; a < limit; a++) {
 		float aFloat = (float)a;
-		oscillators[bankIndex] = aFloat;
-		oscillators[bankIndex + 1] = 1.0 / aFloat;
-		bankIndex += 2;
+		//oscillators[bankIndex] = aFloat;
+		//oscillators[bankIndex + 1] = 1.0 / aFloat;
+		//bankIndex += 2;
+		bool aPrime = true;
 		for (int b = 2; b < a; b++) {
-			float bFloat = (float)b;
-			if (a % b != 0) {
+			if (a % b == 0) {
+				aPrime = false;
+				b = a;
+			}
+		}
+		for (int b = 1; b < a; b++) {
+			bool bPrime = true;
+			for (int c = 2; c < b; c++) {
+				if (b % c == 0) {
+					bPrime = false;
+					c = b;
+				}
+			}
+			if (aPrime || bPrime && a % b != 0) {
+				float bFloat = (float)b;
+				//cout << a << " " << b << endl;
 				oscillators[bankIndex] = aFloat / bFloat;
 				oscillators[bankIndex + 1] = bFloat / aFloat;
 				bankIndex += 2;
@@ -69,7 +111,7 @@ void ofApp::setUniforms() {
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
 	width = (float)ofGetWidth();
 	height = (float)ofGetHeight();
 	frameBuffer.allocate(width, height);
@@ -106,10 +148,10 @@ float ofApp::oscillate(float phase, float phaseIncrement) {
 	return 0.0;
 }
 
-void ofApp::audioOut(ofSoundBuffer &buffer) {
+void ofApp::audioOut(ofSoundBuffer& buffer) {
 	for (int a = 0; a < buffer.getNumFrames(); a++) {
 		//cout << phase << endl;
-		for (int b = 0; b < channels; b++){
+		for (int b = 0; b < channels; b++) {
 			phase += phaseIncrement;
 			phase = fmod(phase, 1.0);
 			sample[b] = lookup(phase);
@@ -118,4 +160,3 @@ void ofApp::audioOut(ofSoundBuffer &buffer) {
 	}
 
 }
-
--- ofApp.h
+++ ofApp.h
@@ -2,32 +2,33 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
-	public:
-		static const int wavetableSize = 256;
-		array<float, wavetableSize> wavetable;
-		constexpr void fillWavetable();
-		static const int degree = 16;
-		constexpr static const int getBankSize(int order);
-		ofShader shader;
-		ofFbo frameBuffer;
-		ofSoundStreamSettings streamSettings;
-		const int sampleRate = 48000;
-		const int bufferSize = 256;
-		const int channels = 2;
-		int bankIndex = 3;
-		ofSoundStream stream;
-		void hardwareSetup();
-		float phase, phaseIncrement;
-		void setup();
-		float width, height;
-		ofVec2f window;
-		void setUniforms();
-		void draw();
-		void ofSoundStreamSetup(ofSoundStreamSettings settings);
-		float averageTwo(float inA, float inB, float mix);
-		float lookup(float phase);
-		float oscillate(float phase, float phaseIncrement);
-		array<float, 2> sample;
-		void audioOut(ofSoundBuffer& audioBuffer);
+class ofApp : public ofBaseApp {
+public:
+	static const int wavetableSize = 256;
+	array<float, wavetableSize> wavetable;
+	static const int limit = 17;
+	array<bool, limit> primes;
+	constexpr void fillWavetable();
+	constexpr static const int getBankSize(int order);
+	ofShader shader;
+	ofFbo frameBuffer;
+	ofSoundStreamSettings streamSettings;
+	const int sampleRate = 48000;
+	const int bufferSize = 256;
+	const int channels = 2;
+	int bankIndex = 1;
+	ofSoundStream stream;
+	void hardwareSetup();
+	float phase, phaseIncrement;
+	void setup();
+	float width, height;
+	ofVec2f window;
+	void setUniforms();
+	void draw();
+	void ofSoundStreamSetup(ofSoundStreamSettings settings);
+	float averageTwo(float inA, float inB, float mix);
+	float lookup(float phase);
+	float oscillate(float phase, float phaseIncrement);
+	array<float, 2> sample;
+	void audioOut(ofSoundBuffer& audioBuffer);
 };
